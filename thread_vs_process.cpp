4a4af517b8b23f3d642c35936cd72acd23ecdcfc
github username <lhon001@gmail.com>
2020-06-26 17:24:44 -0400

    test

--- thread_vs_process.cpp
+++ thread_vs_process.cpp
@@ -26,7 +26,6 @@ int main() {
     // main() infinite while loop to keep main() thread alive
     while (true) {
         std::this_thread::sleep_for(std::chrono::seconds(1));
-    }
-    
+    }  
     return 0;
 }
